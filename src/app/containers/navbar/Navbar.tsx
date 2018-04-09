028a6ea9c1aa8e1d309fdf8952c6ddec876d403a
Mio Engel <mioengel@gmail.com>
2018-04-09 20:58:54 +0200

    Cleanup comment and removed curly bracket

--- src/app/containers/navbar/Navbar.tsx
+++ src/app/containers/navbar/Navbar.tsx
@@ -1,17 +1,14 @@
 import * as classnames from 'classnames'
 import * as React from 'react'
 
-// import { RouteComponentProps, withRouter } from 'react-router'
-
 const styles = require('./Navbar.scss')
 
 class Navbar extends React.Component {
   render() {
-    console.log(this)
     return (
       <ul className={styles.navbarContainer}>
         <li className={styles.navbarItem}>
-          {<a className={styles.navbarLink} href="#home">Home</a>}
+          <a className={styles.navbarLink} href="#home">Home</a>
         </li>
         <li className={styles.navbarItem}>
           <a className={styles.navbarLink} href="#login">Login</a>
