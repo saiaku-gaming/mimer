1ec8d87b2954537a1eb821e63461a1c2692d0c7e
Juni Hoffsten <juni.hoffsten@gmail.com>
2018-04-17 14:43:39 +0200

    ingenting som funkar

--- src/app/containers/userlist/UserListScreen.tsx
+++ src/app/containers/userlist/UserListScreen.tsx
@@ -1,55 +1,67 @@
 import * as React from 'react'
+import Bars from 'react-bar'
+import ReactDOM from 'react-dom'
 
 const styles = require('./UserListScreen.scss')
 
 interface State {
-  name: string | null
+ name: string | null
 }
 
 class UserListScreen extends React.Component<{}, State> {
 
-  constructor(props: any) {
-    super(props)
+ constructor(props: any) {
+   super(props)
 
-    this.state = {
-      name: null
-    }
-  }
+   this.state = {
+     name: null [
+        {label: 'Python', value: 50}
+      ]
+   }
+ }
 
-  handleClick(user: string) {
-    return (e: React.MouseEvent<HTMLSpanElement>) => {
-      if (this.state.name === user) {
-        this.setState({
-          name: null
-        })
-      } else {
-        this.setState({
-          name: user
-        })
-      }
-    }
-  }
+ handleClick(user: string) {
+   return (e: React.MouseEvent<HTMLSpanElement>) => {
+     if (this.state.name === user) {
+       this.setState({
+         name: null
+       })
+     } else {
+       this.setState({
+         name: user
+       })
+     }
+   }
+ }
 
-  render() {
-    const users = ['svante', 'juni', 'blatg', 'johnny']
+ render() {
+   const users = ['Svante', 'Juni', 'Adri', 'Jonte']
 
-    return (
-      <div>
-        <h1>THIS IS A USER LIST</h1>
-        {users.map((user, i) => {
-          return (
-            <div key={i}>
-              <h3 className={styles.user}>USER {i + 1}</h3>
-              <div className={styles.bakgrund}>
-                <span className={styles.namn} onClick={this.handleClick(user)}>{user}</span>
-                {this.state.name === user && <span className={styles.tjena}>Tjena</span>}
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
+   return (
+     <div>
+       <h1>THIS IS A USER LIST</h1>
+       {users.map((user, i) => {
+         return (
+           <div key={i}>
+             <h3 className={styles.user}>USER {i + 1}</h3>
+             <div className={this.state.name === user ? styles.selectbackground : styles.background}>
+               <span className={styles.namn} onClick={this.handleClick(user)}>{user}</span>
+               {this.state.name === user && <div className={styles.blevel}><span className={styles.level}>LEVEL</span></div>}
+             </div>
+               <div className="bar">
+                 <Bars data={this.state.name} makeUppercase={true}/>
+               </div>
+           </div>
+         )
+       })}
+     </div>
+   )
+ }
 }
+
+ReactDOM.render(
+  <UserListScreen />,
+  document.getElementById('root')
+)
 
 export default UserListScreen
